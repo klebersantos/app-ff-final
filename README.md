747ba3f083dcf256621b604595092a2b4a421726
kleber <j.kleber.s@gmail.com>
2018-01-27 10:21:20 -0300

    app

---
+++ README.md
@@ -0,0 +1,1 @@
+# app-ff-final
---
+++ src/envrionment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    site_url : 'http://www.kleberdev.com/app',
+    jogos_url : '/wp-json/wp/v2/jogos',
+    historia_url : '/wp-json/wp/v2/historias',
+    categorias_url : '/wp-json/wp/v2/categories',
+    elenco_url : '/wp-json/wp/v2/elenco',
+    uniforme_url : '/wp-json/wp/v2/uniforme',
+    jwt_url: '/jwt-auth/v1/token'
+}
